5cb0c72122022d40c9066f3fd949e31c9a800607
Niko <nick@paradoxx.dev>
2023-08-27 13:11:04 -0700

    Initial Commit (nothing to see here)

---
+++ CentralBase/CentralBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentralBaseApp.swift
+//  CentralBase
+//
+//  Created by Nicholas Peters on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CentralBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CentralBase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CentralBase
+//
+//  Created by Nicholas Peters on 8/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
